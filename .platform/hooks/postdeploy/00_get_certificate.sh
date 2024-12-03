1c3cc6e62ecb6b1380d525faad425aca6c841e37
Tasedit <tasedit@gmail.com>
2024-12-03 11:12:51 -0700

    all files as of 12/2

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo certbot -n -d 3-1.intex.is404.net --nginx --agree-tos --email tcdavis@byu.edu
+sudo certbot -n -d 3-1intex.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email tcdavis@byu.edu
---
+++ index.js
@@ -0,0 +1,54 @@
+let express = require("express");
+
+let app = express();
+
+let path = require("path");
+
+let security = false;
+
+const port = process.env.PORT || 5500;
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended: true})); // Makes it so the server can get the stuff out of the form //Id is for DOM, Name is for the server
+
+app.use(express.static('public')); // Enabling public folder for css
+
+// Connect the knex library
+const knex = require("knex") ({
+    client : "pg",
+    connection : {
+        host : process.env.RDS_HOSTNAME || "localhost",
+        user : process.env.RDS_USERNAME || "postgres",
+        password : process.env.RDS_PASSWORD || "POS921pos@",
+        database : process.env.RDS_DB_NAME || "disneycharacters",
+        port : process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
+    }
+})
+
+app.get('/', (req, res) => {
+    res.render('landingPage', {title: 'Turtle Shelter Project'});
+});
+
+app.get('/about', (req, res) => {
+    res.render('pages/about', {title : 'About'})
+})
+
+app.get('/how-you-can-help', (req, res) => {
+    res.render('pages/how-you-can-help', {title : 'How You Can Help'})
+})
+
+app.get('/sponsors', (req, res) => {
+    res.render('pages/sponsors', {title : 'Our Sponsors'})
+})
+
+app.get('/login-page', (req, res) => {
+    res.render('pages/login-page', {title : 'Our Sponsors'})
+})
+
+
+// Shows server is listening on start up
+app.listen(port, () => console.log("Listening..."));
